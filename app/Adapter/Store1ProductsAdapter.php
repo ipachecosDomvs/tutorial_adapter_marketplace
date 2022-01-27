edb3fae235cd5e0eed87376fc371f8a40766f88a
Igor Silva <igor.silva@domvsit.com.br>
2022-01-27 13:18:13 -0300

    Mudança no caminho da pasta Adapter

--- app/Adapter/Store1ProductsAdapter.php
+++ app/Adapter/Store1ProductsAdapter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Adapter;
+namespace App\Adapter;
 
 class Store1ProductsAdapter implements ProductsAdapterInterface
 {
--- app/Adapter/Store2ProductsAdapter.php
+++ app/Adapter/Store2ProductsAdapter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Adapter;
+namespace App\Adapter;
 
 class Store2ProductsAdapter implements ProductsAdapterInterface
 {
